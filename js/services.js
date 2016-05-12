22c5f64b1c45e5c2948da12be69a9733433f212e
Devany Moe <devanymoe@gmail.com>
2016-05-12 10:32:26 -0700

    Refactor service

--- js/services.js
+++ js/services.js
@@ -1,18 +1,19 @@
 angular
   .module("myApp")
-  .factory("movieServices", function($http) {
-    var movies;
-    return {
-      returnMovies: function(query) {
-        return $http.get('http://www.omdbapi.com/?s=' + query).then(function(data) {
-          return data.data.Search;
-        });
-      },
-      returnMovieDetails: function(id) {
-        return $http.get('http://www.omdbapi.com/?i=' + id).then(function(data) {
-          return data.data;
-        });
-      }
+  .factory("movieServices", movieService)
 
+function movieService($http) {
+  var movies;
+  return {
+    returnMovies: function(query) {
+      return $http.get('http://www.omdbapi.com/?s=' + query).then(function(data) {
+        return data.data.Search;
+      });
+    },
+    returnMovieDetails: function(id) {
+      return $http.get('http://www.omdbapi.com/?i=' + id).then(function(data) {
+        return data.data;
+      });
     }
-  });
+  }
+};
